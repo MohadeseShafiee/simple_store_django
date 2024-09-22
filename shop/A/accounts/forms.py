0bd3a41b62314f1edc6ace49ab98de928aa3a0fb
MohadeseShafiee <mmohadese.shafieee@gmail.com>
2024-09-22 11:17:27 +0330

    send sms

--- shop/A/accounts/forms.py
+++ shop/A/accounts/forms.py
@@ -42,5 +42,24 @@ class UserChangeForm(forms.ModelForm):
 class UserRegisterationForm(forms.Form):
     email = forms.EmailField()
     full_name = forms.CharField(label='Full name')
-    phone_number = forms.CharField(max_length=11)
+    phone = forms.CharField(max_length=11)
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('This email already exists')
+        return email
+    
+    def clean_phone(self):
+        phone = self.cleaned_data['email']
+        user = User.objects.filter(phone_number=phone).exists()
+        if user:
+            raise ValidationError('This phone already exists')
+        return phone
+
+
+class verifyCodeForm(forms.Form):
+    code = forms.IntegerField()
---
+++ shop/A/utils.py
@@ -0,0 +1,18 @@
+#from kavenegar import *
+
+
+def send_otp_code(phone_number, code):
+    pass
+    # try:
+    #     api = kavenegarAPI('536772766D6E504B4C6834356C4F615A32346E314F7A594E415545544F4C5568706A595642306A476158303D')
+    #     params = {
+    #         'sender': '',
+    #         'receptor': phone_number,
+    #         'message': f'کد تایید شما {code}'
+    #     }
+    #     response = api.sms.send(params)
+    #     print(response)
+    # except APIExeptin as e:
+    #     print(e)
+    # except HTTPExeption as e:
+    #     print(e)
